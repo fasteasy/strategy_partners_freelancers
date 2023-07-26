5334d80f9367cb74f158af3af354a8a0ae727898
Vadim Kochergin <tamato2008@rambler.ru>
2023-07-26 15:41:13 +0300

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <Tutorial/>
+  <div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem accusamus totam necessitatibus quisquam, laboriosam doloribus aspernatur cupiditate inventore repudiandae repellendus, tenetur ex ratione id dolorum enim rerum corrupti libero illum!
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
 }
 </script>
---
+++ plugins/app.js
@@ -0,0 +1,7 @@
+import 'normalize.css'
+import '../assets/fonts/IdealistSans/style.css'
+import '~/assets/styles/variables.css'
+import '~/assets/styles/reset.css'
+
+export default function () {
+}
